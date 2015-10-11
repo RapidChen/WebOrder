6787020d4b01c03f65833434e490cbb76a46a82e
asd220asd <qq378744461@hotmail.com>
2015-10-11 23:20:29 +0800

    WebOrder
    编写了购物车功能，预留了提交订单接口。

--- OrderSystem/urls.py
+++ OrderSystem/urls.py
@@ -12,4 +12,12 @@ urlpatterns = patterns('',
     url(r'^products/(?P<product_pid>[^/]+)/$', 'OrderSystem.views.product_detail', name='product'),
     #历史订单
     url(r'^orderlist/$', views.order, name='orderlist'),
+    #订单详情页面
+    url(r'^orders/(?P<order_listid>[^/]+)/$', 'OrderSystem.views.order_detail', name='order'),
+    #登出
+    url(r'^logout/$', 'OrderSystem.views.logout', name='logout'),
+    #购物车
+    url(r'^cart/$', 'OrderSystem.views.cart', name='cart'),
+    #清空购物车
+    url(r'^empty/$', 'OrderSystem.views.empty', name='empty' ),
 )
--- OrderSystem/views.py
+++ OrderSystem/views.py
@@ -1,6 +1,6 @@
 #coding:utf-8
 from django.shortcuts import render,render_to_response
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect,Http404
 from django.http import HttpResponse
 from .models import *
 from django import forms
@@ -13,6 +13,12 @@ def checkcookie(request):
         return None
     else:
         return username
+
+def iniCart(response):
+    products = Product.objects.all()
+    for product in products:
+        response.set_cookie(str(product.pid), 0)
+    return response
 
 #定义用户表单模型
 class UserForm(forms.Form):
@@ -36,10 +42,14 @@ def login(request):
                 if user.password == password:
                     response = render_to_response('success.html')
                     response.set_cookie('username', username, 3600)
+                    response = iniCart(response)
                     return response
                 else:
                     return render_to_response('login.html', {'userform':userform, 'error':'密码错误！'})
     else:
+        username = checkcookie(request)
+        if username != None:
+            return HttpResponseRedirect('/list/')
         userform = UserForm()
     return render_to_response('login.html', {'userform':userform})
 
@@ -53,12 +63,23 @@ def index(request):
 
 #商品
 def product_detail(request, product_pid):
+    info = ''
     username = checkcookie(request)
     if username == None:
         return HttpResponseRedirect('/')
     product = Product.objects.get(pid = product_pid)
     product.picture = str(product.picture).replace('common_','/')
-    return render(request, 'product.html', {'product': product})
+    response = render(request, 'product.html', {'product': product, 'info':info})
+    if request.method == 'POST':
+        amount = request.POST['amount']
+        if amount ==  '':
+            return response
+        response = render(request, 'success1.html')
+        amount0 = int(request.COOKIES[str(product_pid)])
+        amount = int(amount) + amount0
+        response.set_cookie(str(product_pid), amount)
+        return response
+    return response
 
 #历史订单
 def order(request):
@@ -68,4 +89,70 @@ def order(request):
     orders = OrderList.objects.filter(user__username = username)
     return render(request, 'orderlist.html', {'orders': orders})
 
+#订单详情
+def order_detail(request, order_listid):
+    username = checkcookie(request)
+    if username == None:
+        return HttpResponseRedirect('/')
+    try:
+        order = OrderList.objects.get(listid = order_listid)
+    except:
+        raise Http404
+    if username != order.user.username:
+        raise Http404
+    details = OrderDetail.objects.filter(orderid = order_listid)
+    return render(request, 'order.html', {'order': order, 'details': details})
+
+#登出
+def logout(request):
+    response = HttpResponseRedirect('/')
+    response.delete_cookie('username')
+    return response
+
+#购物车
+def cart(request):
+    cart0 = []
+    username = checkcookie(request)
+    if username == None:
+        return HttpResponseRedirect('/')
+    if request.method == 'POST':
+        selects = []
+        products = Product.objects.all()
+        for product in products:
+            try:
+                selects.append(request.POST[str(product.pid)])
+            except:
+                pass
+        if not selects:
+            return render_to_response('fail.html', {'error':'未选择商品或者购物车为空！'})
+        response = render(request, 'success1.html')
+        for select in selects:
+            response.set_cookie(select, 0)
+        return response
+    products = Product.objects.all()
+    total = 0
+    weight = 0
+    for product in products:
+        amount =  int(request.COOKIES[str(product.pid)])
+        if amount != 0:
+            product.amount = amount
+            total += (product.retail * amount)
+            weight += (product.weight * amount)
+            cart0.append(product)
+        air = weight * Ship.objects.get(shipname='空运').price
+        land = weight * Ship.objects.get(shipname='陆运').price
+    return render(request, 'cart.html', {'products': cart0, 'total': total, 'weight':weight, 'air':air, 'land':land})
+
+def empty(request):
+    username = checkcookie(request)
+    if username == None:
+        return HttpResponseRedirect('/')
+
+    products = Product.objects.all()
+    for product in products:
+        if int(request.COOKIES[str(product.pid)]):
+            response = render(request, 'success1.html')
+            response = iniCart(response)
+            return response
+    return render_to_response('fail.html', {'error':'购物车已经为空！'})
 # Create your views here.
